474f04590283c799654fca90db972fb8e50a11cc
abdulrafay23butt <abdulrafay23butt@gmail.com>
2025-06-18 18:54:56 +0500

    changed server again

--- backend/server.js
+++ backend/server.js
@@ -80,13 +80,3 @@ app.use("/api/updateOrder", updateOrder);
 app.use('/uploads', express.static('uploads'));
 
 
-if (process.env.NODE_ENV === 'production') {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
-
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-  });
-}
